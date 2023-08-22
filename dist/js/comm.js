2a4a706ef18e7567207dd73d830fc6fd066aeca7
김현기 <rlagusrl98@gmail.com>
2023-08-22 18:24:29 +0900

    8.22 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,1 +1,12 @@
-js;
+// JS ZOOM SCROLL METAH WEB
+let bg = document.querySelector('.bg__metah');
+window.addEventListener('scroll', function () {
+  let value = window.scrollY;
+  bg.style.backgroundSize = 30 + value * 1 + "vw";
+  if (value / 1000 > 0.6) {
+    bg.style.opacity = 1.5 - value / 1000;
+    console.log(value / 1000)
+  } else {
+    bg.style.opacity = 1
+  }
+})
