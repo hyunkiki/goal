9059dc9a192116f5a3d1c2d769043adadabe7da0
hyunkiki <rlagusrl98@gmail.com>
2023-08-29 23:18:34 +0900

    8.29 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -6,15 +6,22 @@ var swiper = new Swiper(".mySwiper5", {
     clickable: true,
   },
 });
-var menu = ['축구의 역사', '축구의 포지션', '축구가 인기있는 이유', '축구가 차별받는 이유', '축구의단점', '축구의장점']
-var mySwiper = new Swiper('.swiper-container', {
+var menu = [
+  "축구의 역사",
+  "축구의 포지션",
+  "축구가 인기있는 이유",
+  "축구가 차별받는 이유",
+  "축구의단점",
+  "축구의장점",
+];
+var mySwiper = new Swiper(".swiper-container", {
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
 
     renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (menu[index]) + '</span>';
+      return '<span class="' + className + '">' + menu[index] + "</span>";
     },
   },
-})
+});
