4a3f667414f9562f917e6e63f72d647842f53aeb
김현기 <rlagusrl98@gmail.com>
2023-09-06 18:17:57 +0900

    9.06 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,6 +1,7 @@
 var swiper = new Swiper(".mySwiper5", {
   slidesPerView: 3,
   spaceBetween: 30,
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -16,6 +17,7 @@ var menu = [
 ];
 var mySwiper = new Swiper(".swiper-container", {
   // If we need pagination
+  effect: "fade",
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -44,6 +46,53 @@ var mySwiper = new Swiper(".swiper-container2", {
       return '<span class="' + className + '">' + menu[index] + "</span>";
     },
   },
+});
+
+// const scrollBox = document.getElementById('.scroll_box');
+// const content = document.getElementById('.content1');
+
+// let scrollPosition = 0;
+
+// // scrollBox.addEventListener('.scroll_box', () => {
+// //   const scrollTop = scrollBox.scrollTop;
+// //   if (scrollTop > scrollPosition) {
+// //     // 아래로 스크롤하는 경우: 슬라이드 업
+// //     content.style.transform = `translateY(-${scrollTop}px)`;
+// //   } else {
+// //     // 위로 스크롤하는 경우: 슬라이드 다운
+// //     content.style.transform = `translateY(-${scrollTop}px)`;
+// //   }
+// //   scrollPosition = scrollTop;
+// // });
+// scrollBox.addEventListener('scroll', () => {
+//   const scrollTop = scrollBox.scrollTop;
+//   if (scrollTop > scrollPosition) {
+//     // 아래로 스크롤하는 경우: 슬라이드 업
+//     content.style.transform = `translateY(-${scrollTop}px)`;
+//   } else {
+//     // 위로 스크롤하는 경우: 슬라이드 다운
+//     content.style.transform = `translateY(-${scrollTop}px)`;
+//   }
+//   // scrollPosition을 업데이트합니다.
+//   scrollPosition = scrollTop;
+// });
+
+const scrollBox = document.querySelector('.scroll_box'); // .scroll_box 클래스를 가진 요소를 찾습니다.
+const content = document.querySelector('.content1'); // .content1 클래스를 가진 요소를 찾습니다.
+
+let scrollPosition = 0;
+
+scrollBox.addEventListener('scroll', () => { // 이벤트 이름은 'scroll'로 변경합니다.
+  const scrollTop = scrollBox.scrollTop;
+  if (scrollTop > scrollPosition) {
+    // 아래로 스크롤하는 경우: 슬라이드 업
+    content.style.transform = `translateY(-${scrollTop}px)`;
+  } else {
+    // 위로 스크롤하는 경우: 슬라이드 다운
+    content.style.transform = `translateY(-${scrollTop}px)`;
+  }
+  // scrollPosition을 업데이트합니다.
+  scrollPosition = scrollTop;
 });
 
 AOS.init();
