65c0e13021caa9fdb48f2139afb129b8618527c9
hyunkiki <rlagusrl98@gmail.com>
2023-09-20 23:45:28 +0900

    9.20 업데이트

--- dist/js/contents.js
+++ dist/js/contents.js
@@ -1,4 +1,13 @@
-var swiper = new Swiper(".mySwiper", {});
+// var swiper = new Swiper(".mySwiper", {});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 50,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true
+  // }
+});
 
 // $(function () {
 //   $("foword_player_img").click(function () {
